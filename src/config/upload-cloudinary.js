1523b589a03634e20e9a1c88ef2225927603467d
ana <ana.souza0087@gmail.com>
2025-10-21 22:10:29 -0300

    remove console.log

--- src/config/upload-cloudinary.js
+++ src/config/upload-cloudinary.js
@@ -5,8 +5,6 @@ import { v2 as cloudinary } from "cloudinary"
 import { CloudinaryStorage } from "multer-storage-cloudinary"
 import multer from "multer"
 
-console.log("Cloudinary API_KEY:", process.env.CLOUDINARY_API_KEY)
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
